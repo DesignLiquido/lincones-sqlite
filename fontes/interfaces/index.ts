f788f58228ad8b1e9a7ad6e7e532ad0dc34ccccc
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2022-12-11 16:10:17 -0800

    Ajustes no cliente do SQLite. Esboço do tradutor.

--- fontes/interfaces/index.ts
+++ fontes/interfaces/index.ts
@@ -1,4 +1,3 @@
 export { ILincones } from './lincones-interface';
-export { ISqlite } from './sqlite-interface';
 export { AvaliadorSintaticoInterface } from './avaliador-sintatico-interface';
 export * from './simbolo-interface';
--- fontes/tradutor/index.ts
+++ fontes/tradutor/index.ts
@@ -1,3 +1,42 @@
+import { Comando } from "../comandos";
+
 export class Tradutor {
-    
+
+    traduzirComandoAtualizar() {
+        return '';
+    }
+
+    traduzirComandoCriar() {
+        return '';
+    }
+
+    traduzirComandoExcluir() {
+        return '';
+    }
+
+    traduzirComandoInserir() {
+        return '';
+    }
+
+    traduzirComandoSelecionar() {
+        return '';
+    }
+
+    dicionarioComandos = {
+        Atualizar: this.traduzirComandoAtualizar.bind(this),
+        Criar: this.traduzirComandoCriar.bind(this),
+        Excluir: this.traduzirComandoExcluir.bind(this),
+        Inserir: this.traduzirComandoInserir.bind(this),
+        Selecionar: this.traduzirComandoSelecionar.bind(this)
+    }
+
+    traduzir(comandos: Comando[]) {
+        let resultado = '';
+
+        for (const comando of comandos) {
+            resultado += `${this.dicionarioComandos[comando.constructor.name](comando)} \n`;
+        }
+
+        return resultado;
+    }
 }
