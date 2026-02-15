8f3f82d573f4476855efc0cfcce2a23413d99122
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-02-15 13:10:27 -0800

    Adiciona suporte para SE NÃO EXISTIR no comando CREATE TABLE

--- fontes/avaliador-sintatico/avaliador-sintatico.ts
+++ fontes/avaliador-sintatico/avaliador-sintatico.ts
@@ -1,10 +1,12 @@
-import { Comando } from '../comum/fontes/comandos';
+import { Comando, Criar } from '../comum/fontes/comandos';
 import {
     RetornoAvaliadorSintatico,
     RetornoLexador
 } from '../comum/fontes/interfaces/retornos';
-import tiposDeSimbolos from '../comum/fontes/tipos-de-simbolos';
 import { AvaliadorSintaticoBase } from '../comum/fontes/avaliador-sintatico/avaliador-sintatico-base';
+import { Coluna } from '../comum/fontes/construtos';
+
+import tiposDeSimbolos from '../comum/fontes/tipos-de-simbolos';
 
 export class AvaliadorSintatico extends AvaliadorSintaticoBase {
     override declaracao() {
@@ -23,6 +25,65 @@ export class AvaliadorSintatico extends AvaliadorSintaticoBase {
                 this.avancar();
                 return null;
         }
+    }
+
+    override comandoCriar(): Criar {
+        // Essa linha nunca deve retornar erro.
+        this.consumir(
+            tiposDeSimbolos.CRIAR,
+            'Esperado palavra reservada "CRIAR".'
+        );
+
+        // Essa linha nunca deve retornar erro.
+        const simboloTabela = this.consumir(
+            tiposDeSimbolos.TABELA,
+            'Esperado palavra reservada "TABELA".'
+        );
+
+        let seNaoExistir = false;
+        if (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.SE)) {
+            this.consumir(
+                tiposDeSimbolos.NAO,
+                'Esperado palavra reservada "NÃO" após palavra reservada "SE".'
+            );
+
+            this.consumir(
+                tiposDeSimbolos.EXISTIR,
+                'Esperado palavra reservada "EXISTIR" após palavra reservada "NÃO".'
+            );
+
+            seNaoExistir = true;
+        }
+
+        const nomeDaTabela = this.consumir(
+            tiposDeSimbolos.IDENTIFICADOR,
+            'Esperado identificador de nome de tabela após palavra reservada "TABELA".'
+        );
+
+        this.consumir(
+            tiposDeSimbolos.PARENTESE_ESQUERDO,
+            'Esperado abertura de parênteses após nome da tabela'
+        );
+
+        const colunas: Coluna[] = [];
+
+        do {
+            colunas.push(this.comandoCriacaoColuna());
+        } while (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.VIRGULA));
+
+        this.consumir(
+            tiposDeSimbolos.PARENTESE_DIREITO,
+            'Esperado fechamento de parênteses após nome da tabela'
+        );
+
+        this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.PONTO_VIRGULA);
+
+        return new Criar(
+            simboloTabela.linha,
+            nomeDaTabela.lexema,
+            colunas,
+            seNaoExistir
+        );
     }
 
     analisar(retornoLexador: RetornoLexador): RetornoAvaliadorSintatico {
